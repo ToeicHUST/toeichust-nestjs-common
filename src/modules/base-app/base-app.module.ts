0a6e0605ea0cefc274d5004e3586ec89f19bb586
VuVanNghia <vuvannghia.work@gmail.com>
2026-01-08 01:37:53 +0700

    update base-app.module.ts

--- src/modules/base-app/base-app.module.ts
+++ src/modules/base-app/base-app.module.ts
@@ -1,9 +1,7 @@
 import { Module } from '@nestjs/common';
-import { BaseAppService } from './base-app.service';
 import { BaseAppController } from './base-app.controller';
 
 @Module({
   controllers: [BaseAppController],
-  providers: [BaseAppService],
 })
 export class BaseAppModule {}
